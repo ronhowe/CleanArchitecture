6f28c5a1a27959d19e4b6e2e04da146fe983f9ad
Ron Howe <ronhowe@hotmail.com>
2021-03-14 12:22:02 -0400

    Updated NuGet Packages Targeted localhost for Database Connections

--- src/WebUI/Startup.cs
+++ src/WebUI/Startup.cs
@@ -126,8 +126,8 @@ namespace CleanArchitecture.WebUI
 
                 if (env.IsDevelopment())
                 {
-                    //spa.UseAngularCliServer(npmScript: "start");
-                    spa.UseProxyToSpaDevelopmentServer(Configuration["SpaBaseUrl"] ?? "http://localhost:4200");
+                    spa.UseAngularCliServer(npmScript: "start");
+                    //spa.UseProxyToSpaDevelopmentServer(Configuration["SpaBaseUrl"] ?? "http://localhost:4200");
                 }
             });
         }
