f25a9ddf6ec09490fc623e057352042518035024
Ron Howe <ronhowe@hotmail.com>
2021-03-14 19:25:18 -0400

    Added NCrunch Support

---
+++ tests/POST/POST.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace POST
+{
+    [TestClass]
+    public class POST
+    {
+        [TestMethod]
+        public void P0ST()
+        {
+            Debug.WriteLine("POST");
+            Assert.IsTrue(true);
+        }
+    }
+}
