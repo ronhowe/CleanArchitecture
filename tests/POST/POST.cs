7dddb065eaf5e703c6dfc5075805c1970860d1f6
Ron Howe <ronhowe@hotmail.com>
2021-03-14 20:24:22 -0400

    Added HTTP GET in PostBoot

--- tests/POST/POST.cs
+++ tests/POST/POST.cs
@@ -1,5 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
 
 namespace POST
 {
@@ -10,13 +13,25 @@ namespace POST
         public void Post()
         {
             Debug.WriteLine("POST");
+            Debug.WriteLine(DateTime.Now);
             Assert.IsTrue(true);
             PostBoot();
         }
 
         private static void PostBoot()
         {
-            Debug.WriteLine("your code here");
+            HttpClient client = new HttpClient();
+
+            try
+            {
+                HttpResponseMessage response = client.GetAsync("https://localhost:5001/api/WeatherForecast").Result;
+
+                response.EnsureSuccessStatusCode();
+            }
+            catch (HttpRequestException e)
+            {
+                Debug.WriteLine(e.Message);
+            }
         }
     }
 }
