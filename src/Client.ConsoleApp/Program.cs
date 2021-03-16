465f3ffc99d7dc8f71996b61281e689b662ed8e3
Ron Howe <ronhowe@hotmail.com>
2021-03-16 10:22:01 -0400

    MVP ConsoleApp

--- src/Client.ConsoleApp/Program.cs
+++ src/Client.ConsoleApp/Program.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using Microsoft.Identity.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using POST;
+using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading;
+using System.Timers;
 
 namespace Client.ConsoleApp
 {
@@ -9,34 +15,163 @@ namespace Client.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Uri uri = new Uri("https://localhost:5001/health");
-
-            Console.Title = uri.ToString();
-
             Refresh(ConsoleColor.DarkYellow);
 
-            HttpClient client = new();
+            ///////////////////////////////////////////////////////
+            #region POST
+
+            Trace.WriteLine($"post={DateTime.Now.ToString()}");
+
+            #endregion POST
+            ///////////////////////////////////////////////////////
+
+            ///////////////////////////////////////////////////////
+            #region CONFIGURATION
+
+            AuthConfig config = AuthConfig.ReadFromJsonFile("appsettings.json");
+
+            Trace.WriteLine($"config.Authority={config.Authority}");
+            Trace.WriteLine($"config.BaseAddress={config.BaseAddress}");
+            Trace.WriteLine($"config.ClientId={config.ClientId}");
+            Trace.WriteLine($"config.ClientSecret={config.ClientSecret}");
+            Trace.WriteLine($"config.ResourceID={config.ResourceID}");
+            Trace.WriteLine($"config.TenantId={config.TenantId}");
+
+            Uri uri = new Uri(config.BaseAddress);
+            HttpClient anonymousHttpClient = new HttpClient();
+            Stopwatch stopWatch = new Stopwatch();
+            TimeSpan ts;
+            string elapsed;
+
+            #endregion CONFIGURATION
+            ///////////////////////////////////////////////////////
 
             while (true)
             {
                 try
                 {
-                    HttpResponseMessage response = client.GetAsync(uri).Result;
+                    ///////////////////////////////////////////////////////
+                    #region UNAUTHENTICATED
 
-                    response.EnsureSuccessStatusCode();
+                    SetTimer();
+
+                    stopWatch.Start();
+
+                    Trace.WriteLine($"anonymous_request_start={DateTime.Now.ToString()}");
+                    HttpResponseMessage response = anonymousHttpClient.GetAsync(uri).Result;
+                    Trace.WriteLine($"anonymous_request_stop={DateTime.Now.ToString()}");
+
+                    stopWatch.Stop();
+
+                    aTimer.Stop();
+
+                    aTimer.Dispose();
+
+                    ts = stopWatch.Elapsed;
+
+                    elapsed = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+
+                    Trace.WriteLine($"anonymous_request_elapsed={elapsed}");
+
+                    Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.Unauthorized);
+
+                    #endregion UNAUTHENTICATED
+                    ///////////////////////////////////////////////////////
+
+                    ///////////////////////////////////////////////////////
+                    #region AUTHENTICATE
+
+                    IConfidentialClientApplication app;
+
+                    app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                        .WithClientSecret(config.ClientSecret)
+                        .WithAuthority(new Uri(config.Authority))
+                        .Build();
+
+                    string[] ResourceIds = new string[] { config.ResourceID };
+
+                    AuthenticationResult result = null;
+
+                    SetTimer();
+
+                    stopWatch.Start();
+
+                    Trace.WriteLine($"_request_start={DateTime.Now.ToString()}");
+                    result = app.AcquireTokenForClient(ResourceIds).ExecuteAsync().Result;
+                    Trace.WriteLine($"token_request_stop={DateTime.Now.ToString()}");
+
+                    stopWatch.Stop();
+
+                    aTimer.Stop();
+
+                    aTimer.Dispose();
+
+                    ts = stopWatch.Elapsed;
+
+                    elapsed = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+
+                    Trace.WriteLine($"token_request_elapsed={elapsed}");
+
+                    Assert.IsFalse(string.IsNullOrEmpty(result.AccessToken));
+
+                    Trace.WriteLine($"token={result.AccessToken}");
+
+                    #endregion AUTHENTICATE
+                    ///////////////////////////////////////////////////////
+
+                    ///////////////////////////////////////////////////////
+                    #region AUTENTICATED
+
+                    var authenticatedHttpClient = new HttpClient();
+                    var defaultRequestHeaders = authenticatedHttpClient.DefaultRequestHeaders;
+
+                    if (defaultRequestHeaders.Accept == null ||
+                            !defaultRequestHeaders.Accept.Any(m => m.MediaType == "application/json"))
+                    {
+                        authenticatedHttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    }
+
+                    defaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", result.AccessToken);
+
+                    SetTimer();
+
+                    stopWatch.Start();
+
+                    Trace.WriteLine($"authenticated_request_start={DateTime.Now.ToString()}");
+                    response = authenticatedHttpClient.GetAsync(config.BaseAddress).Result;
+                    Trace.WriteLine($"authenticated_request_stop={DateTime.Now.ToString()}");
+
+                    stopWatch.Stop();
+
+                    aTimer.Stop();
+
+                    aTimer.Dispose();
+
+                    ts = stopWatch.Elapsed;
+
+                    elapsed = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+
+                    Trace.WriteLine($"authenticated_request_elapsed={elapsed}");
+
+                    Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
+
+                    string content = response.Content.ReadAsStringAsync().Result;
+                    Trace.WriteLine($"content={content}");
+
+                    #endregion AUTENTICATED
+                    ///////////////////////////////////////////////////////
 
                     Refresh(ConsoleColor.DarkGreen);
                 }
                 catch (Exception e)
                 {
-                    Debug.WriteLine(e.Message);
+                    Trace.TraceError(e.Message);
 
                     Refresh(ConsoleColor.DarkRed);
                 }
 
                 Thread.Sleep(1000);
             }
-
         }
 
         private static void Refresh(ConsoleColor color)
@@ -44,6 +179,22 @@ namespace Client.ConsoleApp
             Console.BackgroundColor = color;
 
             Console.Clear();
+        }
+
+        private static System.Timers.Timer aTimer;
+
+        private static void SetTimer()
+        {
+            aTimer = new System.Timers.Timer(1000);
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            Trace.WriteLine($"timer={DateTime.Now.ToString()}");
+            //Trace.WriteLine("OnTimedEvent @ {0:HH:mm:ss.fff}", e.SignalTime);
         }
     }
 }
