6ba42ad8aebbe214f3d16a24f23de4c28657e00e
Ron Howe <ronhowe@hotmail.com>
2021-03-27 12:41:50 -0400

    Hygiene

--- src/Server.WebApplication/Program.cs
+++ src/Server.WebApplication/Program.cs
@@ -12,7 +12,6 @@ namespace Server.WebApplication
         {
             CreateHostBuilder(args).Build().Run();
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
